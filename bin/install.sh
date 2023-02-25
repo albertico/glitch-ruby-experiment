19cb73f1376685b55760fc2f302d263e31d8d233
Alberto Colón Viera <aacv@albertico.dev>
2023-02-25 00:06:53 -0500

    Adding echo statements to 'bin/install.sh'

--- bin/install.sh
+++ bin/install.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
+echo "Downloading ruby-build package..."
 wget -O "/tmp/ruby-build.tar.gz" "https://github.com/rbenv/ruby-build/archive/refs/tags/v20230222.tar.gz"
+
 pushd /tmp
+echo "Extracting ruby-build package..."
 tar -xzf ruby-build.tar.gz
+echo "Installing ruby-build..."
 PREFIX=/usr/local ./ruby-build-*/install.sh
 popd
-mkdir /usr/local/rubies/ruby-3.0.5
-ruby-build 3.0.5 /usr/local/rubies/ruby-3.0.5
 
+echo "Creating directory '/usr/local/rubies'..."
+mkdir /usr/local/rubies/ruby-3.0.5
+echo "Installing Ruby v3.0.5 to '/usr/local/rubies/ruby-3.0.5'..."
+ruby-build 3.0.5 /usr/local/rubies/ruby-3.0.5
+echo "Linking Ruby binaries..."
 ln -s /usr/local/rubies/ruby-3.0.5/bin/ruby /usr/local/bin/ruby
 ln -s /usr/local/rubies/ruby-3.0.5/bin/gem /usr/local/bin/gem
 
+echo "Updating RubyGems..."
 gem update --system
+echo "Updating default gems..."
 gem update
 
 pushd ../
+echo "Running 'bundle install'..."
 bundle install
 popd
+
+echo "Done!"
